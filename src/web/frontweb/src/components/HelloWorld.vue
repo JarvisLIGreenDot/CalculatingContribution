6c9ecd9c06f71a5d3394cd105d1b3eb00b3802cd
jarvis.li <jarvis.li@greendotcorp.com>
2025-02-20 23:48:58 +0800

    update index page

--- src/web/frontweb/src/components/HelloWorld.vue
+++ src/web/frontweb/src/components/HelloWorld.vue
@@ -1,33 +1,86 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container fluid>
+    <v-row>
+      <!-- 左侧菜单 -->
+      <v-col cols="3">
+        <v-navigation-drawer permanent app width="250">
+          <v-list nav dense>
+            <v-list-item-group>
+              <v-list-item v-for="(item, index) in menuItems" :key="index" :to="item.route">
+                <v-list-item-icon>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-navigation-drawer>
+      </v-col>
+
+      <!-- 右侧主要内容 -->
+      <v-col cols="9">
+        <v-card>
+          <v-card-title>{{ msg }}</v-card-title>
+          <v-card-text>
+            For a guide and recipes on how to configure / customize this project,<br>
+            check out the 
+            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+          </v-card-text>
+
+          <v-divider></v-divider>
+
+          <!-- Installed CLI Plugins -->
+          <v-card-subtitle>Installed CLI Plugins</v-card-subtitle>
+          <v-list dense>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <!-- Essential Links -->
+          <v-card-subtitle>Essential Links</v-card-subtitle>
+          <v-list dense>
+            <v-list-item v-for="(item, index) in essentialLinks" :key="index">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <a :href="item.href" target="_blank" rel="noopener">{{ item.title }}</a>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <!-- Ecosystem -->
+          <v-card-subtitle>Ecosystem</v-card-subtitle>
+          <v-list dense>
+            <v-list-item v-for="(item, index) in ecosystemLinks" :key="index">
+              <v-list-item-content>
+                <v-list-item-title>
+                  <a :href="item.href" target="_blank" rel="noopener">{{ item.title }}</a>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -35,24 +88,33 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      menuItems: [
+        { title: '菜单项 1', icon: 'mdi-view-dashboard', route: '/' },
+        { title: '菜单项 2', icon: 'mdi-account', route: '/profile' },
+        { title: '菜单项 3', icon: 'mdi-settings', route: '/settings' }
+      ],
+      essentialLinks: [
+        { title: 'Core Docs', href: 'https://vuejs.org' },
+        { title: 'Forum', href: 'https://forum.vuejs.org' },
+        { title: 'Community Chat', href: 'https://chat.vuejs.org' },
+        { title: 'Twitter', href: 'https://twitter.com/vuejs' },
+        { title: 'News', href: 'https://news.vuejs.org' }
+      ],
+      ecosystemLinks: [
+        { title: 'vue-router', href: 'https://router.vuejs.org' },
+        { title: 'vuex', href: 'https://vuex.vuejs.org' },
+        { title: 'vue-devtools', href: 'https://github.com/vuejs/vue-devtools#vue-devtools' },
+        { title: 'vue-loader', href: 'https://vue-loader.vuejs.org' },
+        { title: 'awesome-vue', href: 'https://github.com/vuejs/awesome-vue' }
+      ]
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+/* 可根据需要添加自定义样式 */
 </style>
